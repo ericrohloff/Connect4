22142239e8df535ede15658ecb5d35c3724dca49
Eric Rohloff <121261577+ericrohloff@users.noreply.github.com>
2024-04-20 17:01:21 -0400

    working game and screen

--- game.c
+++ game.c
@@ -24,17 +24,52 @@ void init_board()
     }
 }
 
+void draw_logo()
+{
+
+    printf("        CCCCCCCCCCCCC     OOOOOOOOO                                                                                          tttt                  ffffffffffffffff\n");
+    printf("     CCC::::::::::::C   OO:::::::::OO                                                                                     ttt:::t                 f::::::::::::::::f\n");
+    printf("   CC:::::::::::::::C OO:::::::::::::OO                                                                                   t:::::t                f::::::::::::::::::f\n");
+    printf("  C:::::CCCCCCCC::::CO:::::::OOO:::::::O                                                                                  t:::::t                f::::::fffffff:::::f\n");
+    printf(" C:::::C       CCCCCCO::::::O   O::::::Onnnn  nnnnnnnn    nnnn  nnnnnnnn        eeeeeeeeeeee        ccccccccccccccccttttttt:::::ttttttt          f:::::f       ffffffooooooooooo   uuuuuu    uuuuuu rrrrr   rrrrrrrrr\n");
+    printf("C:::::C              O:::::O     O:::::On:::nn::::::::nn  n:::nn::::::::nn    ee::::::::::::ee    cc:::::::::::::::ct:::::::::::::::::t          f:::::f           oo:::::::::::oo u::::u    u::::u r::::rrr:::::::::r\n");
+    printf("C:::::C              O:::::O     O:::::On::::::::::::::nn n::::::::::::::nn  e::::::eeeee:::::ee c:::::::::::::::::ct:::::::::::::::::t         f:::::::ffffff    o:::::::::::::::ou::::u    u::::u r:::::::::::::::::r\n");
+    printf("C:::::C              O:::::O     O:::::Onn:::::::::::::::nnn:::::::::::::::ne::::::e     e:::::ec:::::::cccccc:::::ctttttt:::::::tttttt         f::::::::::::f    o:::::ooooo:::::ou::::u    u::::u rr::::::rrrrr::::::r\n");
+    printf("C:::::C              O:::::O     O:::::O  n:::::nnnn:::::n  n:::::nnnn:::::ne:::::::eeeee::::::ec::::::c     ccccccc      t:::::t               f::::::::::::f    o::::o     o::::ou::::u    u::::u  r:::::r     r:::::r\n");
+    printf("C:::::C              O:::::O     O:::::O  n::::n    n::::n  n::::n    n::::ne:::::::::::::::::e c:::::c                   t:::::t               f:::::::ffffff    o::::o     o::::ou::::u    u::::u  r:::::r     rrrrrrr\n");
+    printf("C:::::C              O:::::O     O:::::O  n::::n    n::::n  n::::n    n::::ne::::::eeeeeeeeeee  c:::::c                   t:::::t                f:::::f          o::::o     o::::ou::::u    u::::u  r:::::r\n");
+    printf(" C:::::C       CCCCCCO::::::O   O::::::O  n::::n    n::::n  n::::n    n::::ne:::::::e           c::::::c     ccccccc      t:::::t    tttttt      f:::::f          o::::o     o::::ou:::::uuuu:::::u  r:::::r\n");
+    printf("  C:::::CCCCCCCC::::CO:::::::OOO:::::::O  n::::n    n::::n  n::::n    n::::ne::::::::e          c:::::::cccccc:::::c      t::::::tttt:::::t     f:::::::f         o:::::ooooo:::::ou:::::::::::::::uur:::::r\n");
+    printf("   CC:::::::::::::::C OO:::::::::::::OO   n::::n    n::::n  n::::n    n::::n e::::::::eeeeeeee   c:::::::::::::::::c      tt::::::::::::::t     f:::::::f         o:::::::::::::::o u:::::::::::::::ur:::::r\n");
+    printf("    CCC::::::::::::C   OO:::::::::OO     n::::n    n::::n  n::::n    n::::n  ee:::::::::::::e    cc:::::::::::::::c        tt:::::::::::tt     f:::::::f          oo:::::::::::oo   uu::::::::uu:::ur:::::r\n");
+    printf("       CCCCCCCCCCCCC     OOOOOOOOO       nnnnnn    nnnnnn  nnnnnn    nnnnnn    eeeeeeeeeeeeee      cccccccccccccccc          ttttttttttt       fffffffff            ooooooooooo       uuuuuuuu  uuuurrrrrrr\n");
+    printf("\n");
+    printf("Created by: Eric Rohloff, Josh Wilkie, Abe Nelson\n");
+    printf("\n");
+    printf("\n");
+}
+
 // Method to display baord onto the LCD screen
 void disp_board(int board[ROWS][COLS])
 {
+    printf("  0   1   2   3   4   5   6\n");
+    printf("+---+---+---+---+---+---+---+\n");
     for (int i = 0; i < ROWS; i++)
     {
         for (int j = 0; j < COLS; j++)
         {
-            printf("%d ", board[i][j]);
+            // Move cursor to specific location for each cell
+            printf("|");
+            if (board[i][j] == 0)
+                printf("   ");
+            else if (board[i][j] == 1)
+                printf(" X ");
+            else if (board[i][j] == 2)
+                printf(" O ");
         }
-        printf("\n");
+        printf("|\n");
     }
+    printf("+---+---+---+---+---+---+---+\n");
 }
 
 int check_win(int row, int col, int board[ROWS][COLS])
@@ -246,26 +281,39 @@ int set_piece(int col, int player, int board[ROWS][COLS])
 
 int main()
 {
+    // Initialize the game board
     init_board();
-    printf("%d", set_piece(0, 1, board));
-    printf("\n");
-    printf("%d", set_piece(1, 2, board));
-    printf("\n");
-    printf("%d", set_piece(1, 1, board));
-    printf("\n");
-    printf("%d", set_piece(2, 2, board));
-    printf("\n");
-    printf("%d", set_piece(2, 2, board));
-    printf("\n");
-    printf("%d", set_piece(2, 1, board));
-    printf("\n");
-    printf("%d", set_piece(3, 2, board));
-    printf("\n");
-    printf("%d", set_piece(3, 2, board));
-    printf("\n");
-    printf("%d", set_piece(3, 2, board));
-    printf("\n");
-    printf("%d", set_piece(3, 1, board));
-    printf("\n");
+    draw_logo();
     disp_board(board);
+    printf("\n");
+    printf("Player 1 is X, Player 2 is O\n");
+    char turn = 1;
+    // Check win condition
+    while (1)
+    {
+        int col;
+        printf("Player %d, enter a column: ", turn);
+        scanf("%d", &col);
+        int win = set_piece(col, turn, board);
+        disp_board(board);
+        if (win == 1)
+        {
+            printf("Player 1 wins!\n");
+            break;
+        }
+        else if (win == 2)
+        {
+            printf("Player 2 wins!\n");
+            break;
+        }
+        if (turn == 1)
+        {
+            turn = 2;
+        }
+        else
+        {
+            turn = 1;
+        }
+    }
+    return 0;
 }
